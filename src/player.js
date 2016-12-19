f64689615fa86d4ea8e525df977f8196d6040389
Trish Griego <pmgriego@gmail.com>
2016-12-19 12:37:22 -0800

    added testing backbone models

--- src/player.js
+++ src/player.js
@@ -8,40 +8,48 @@ const Player = Backbone.Model.extend({
   initialize: function(options){
     this.set("name", options.name);
     this.set("plays", []);
-
-    // scrabble is never going to change. Thus, 
+    console.log("options", options);
+    // scrabble is never going to change. Thus,
     this.scrabble = new Scrabble();
+  },
+
+  hasWon: function() {
+    return this.totalScore() > 100;
+  },
+
+  totalScore: function() {
+    var total = 0;
+    for (var i = 0; i < this.get("plays").length; i++){
+      total += this.scrabble.score(this.get("plays")[i]);
+    }
+    return total;
+  },
+
+  highestScoringWord: function() {
+    return this.scrabble.highestScoreFrom(this.get("plays"));
+  },
+
+  highestWordScore: function() {
+    return this.scrabble.score(this.scrabble.highestScoreFrom(this.get("plays")));
+  },
+
+  play: function(word) {
+    if (this.hasWon()){
+      return false;
+    } else {
+      this.get("plays").push(word);
+    }
   }
+
 
 }); // end of Backbone model extend.
 
 
-Player.prototype.hasWon = function() {
-  return this.totalScore() > 100;
-};
 
-Player.prototype.totalScore = function() {
-  var total = 0;
-  for (var i = 0; i < this.plays.length; i++){
-    total += this.scrabble.score(this.plays[i]);
-  }
-  return total;
-};
 
-Player.prototype.highestScoringWord = function() {
-  return this.scrabble.highestScoreFrom(this.plays);
-};
 
-Player.prototype.highestWordScore = function() {
-  return this.scrabble.score(this.scrabble.highestScoreFrom(this.plays));
-};
 
-Player.prototype.play = function(word) {
-  if (this.hasWon()){
-    return false;
-  } else {
-    this.plays.push(word);
-  }
-};
+
+
 
 export default Player;
